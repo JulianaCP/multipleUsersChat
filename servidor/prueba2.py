b2945637c7d8d87f0ac5535d8d096db861a29e3f
Juliana Campos <JCP27@hotmail.com>
2018-03-06 21:17:06 -0600

    la numero 4

--- servidor/prueba2.py
+++ servidor/prueba2.py
@@ -1,15 +1,26 @@
 import socket
 import sys
+import webbrowser
+
 
 def menu():
     print("*************        MENU PRINCIPAL      *******************")
-    HOST, PORT = "172.24.44.219", 8090
+    HOST, PORT = "172.24.43.158", 8090
     data = input("1. Consultar el nombre del host del Servidor\n"
-                "2. Consultar la IP del Servidor\n"
-                "3. Consultar la cantidad de procesos ejecutandose en el servidor\n"
-                "4.Dar la hora en otro pais indicando el paıs\n"
-                "5. Poder enviar mensajes entre cliente y servidor\n"
-                "Opción: ")
+                 "2. Consultar la IP del Servidor\n"
+                 "3. Consultar la cantidad de procesos ejecutandose en el servidor\n"
+                 "4. Dar la hora en otro pais indicando el paıs\n"
+                 "5. Poder enviar mensajes entre cliente y servidor\n"
+                 "Opción: ")
+
+    if(data == '4'):
+        opcion = input("1. Revisar Codigos paises\n"
+                     "2. Escribir Codigo\n"
+                     "Opción: ")
+        if(opcion == '1'):
+            webbrowser.open('https://timezonedb.com/country-codes')
+
+        data = input("Escribir Codigo: ")
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
