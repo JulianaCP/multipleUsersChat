6971599ef96e7bea20805a5bd0d2c82a59db1267
bryan-ruiz <bryanrb95@gmail.com>
2018-03-09 22:47:19 -0600

    5
    opciones listas

--- servidor/cliente.py
+++ servidor/cliente.py
@@ -5,6 +5,7 @@ import webbrowser
 def menu():
     HOST, PORT = "172.24.73.178", 9090
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    code = ""
     while(True):
         print("*************        MENU PRINCIPAL      *******************")
         data = input("1. Consultar el nombre del host del Servidor\n"
@@ -14,26 +15,27 @@ def menu():
                     "5. Poder enviar mensajes entre cliente y servidor\n"
                     "6. Salir\n"
                     "Opción: ")
-
+        if (data == ""):
+            menu()
         if (data == "6"):
             break
-
+        if (data == "5"):
+            print("¿servidor estas ahi?")
         if (data == '4'):
             opcion = input("1. Revisar Codigos paises\n"
                            "2. Escribir Codigo\n"
                            "Opción: ")
-            if (opcion == '1'):
+            if (opcion == ""):
+                menu()
+            elif (opcion == '1'):
                 webbrowser.open('https://timezonedb.com/country-codes')
-
-            codigo = input("Escribir Codigo: ")
-
-        data = data + ",CR"
+            code = input("Escribir Codigo: ")
+        data = data + "," + code
         sock.sendto(data.encode(), (HOST, PORT))
         received = sock.recv(1024)
         print("************************************************************")
         print("Sent:     {}".format(data))
         print("Received: {}".format(received.decode()))
         print("************************************************************")
-
 
 menu()
--- servidor/servidor.py
+++ servidor/servidor.py
@@ -1,5 +1,8 @@
 import socketserver, threading, time
 import socket
+import urllib.request
+import json
+import datetime
 
 HOST, PORT = "172.24.73.178", 9090
 host_name = "Host name: %s" % socket.gethostname()
@@ -11,31 +14,33 @@ class ThreadedUDPRequestHandler(socketserver.BaseRequestHandler):
     def handle(self):
         global numeroDeDatos
         data = self.request[0].strip()
-        opcion= data[0]
         socket = self.request[1]
-        print("22222------222222")
-        print(data)
-        x,y = data.decode().split(",")
-        print(x)
-        print(y)
-        print("------------------")
-        print(opcion)
+        number,code = data.decode().split(",")
+        print(number)
+        print(code)
         current_thread = threading.current_thread()
         print("{}: client: {}, wrote: {}".format(current_thread.name, self.client_address, data))
-
         numeroDeDatos += 1
-
-        if (opcion == 49): #1
+        if (number == "1"): #1
             socket.sendto(host_name.encode(), self.client_address)
-        elif (opcion == 50): #2
+        elif (number == "2"): #2
             socket.sendto(ipAddress.encode(), self.client_address)
-        elif (opcion == 51): #3
+        elif (number == "3"): #3
             datos= str(numeroDeDatos)
             socket.sendto(datos.encode(), self.client_address)
-        else:
-            socket.sendto(host_name.encode(), self.client_address)
-            socket.sendto(data.upper(), self.client_address)
-
+        elif (number == "4"):
+            contents = urllib.request.urlopen(
+                "http://api.timezonedb.com/v2/list-time-zone?key=6I35P7039N8L&format=json&country="+code).read().decode(
+                'utf8')
+            wjdata = json.loads(contents)
+            unix_time = wjdata['zones'][0]['timestamp']
+            variable = datetime.datetime.utcfromtimestamp(
+                int(unix_time)
+            ).strftime('%Y-%m-%d %H:%M:%S')
+            print(variable)
+            socket.sendto(variable.encode(), self.client_address)
+        elif (number == "5"):
+            socket.sendto("Hola! el servidor esta feliz de verte".encode(), self.client_address)
         numeroDeDatos -= 1
 
 class ThreadedUDPServer(socketserver.ThreadingMixIn, socketserver.UDPServer):
